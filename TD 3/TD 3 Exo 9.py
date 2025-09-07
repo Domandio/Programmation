129c8b5ca524dc881c18cf1b5010c297bc129721
Domandio <m.landuze@gmail.com>
2025-09-07 18:10:37 +0200

    .

---
+++ TD 3/TD 3 Exo 9.py
@@ -0,0 +1,28 @@
+#Saisie utilisateur
+nbnotes = int(input("Entrez le nombre de notes que vous voulez saisir :\n"))
+print("----------")
+
+#Variables
+note = 0
+somme = 0
+moy = 0
+notesup = 0
+i = 0
+tab = []
+
+#Boucle For : Saisie des notes dans la liste
+for i in range(nbnotes):
+    note = float(input(f"Saisissez la note n°{i+1}\n"))
+    tab.append(note)
+    somme = somme + tab[i]
+
+#Calcul moyenne
+moy = somme / nbnotes
+
+for i in range(nbnotes):
+    if tab[i] > moy :
+        notesup = notesup + 1
+
+#Affichage final
+print("----------")
+print(f"Le nombre de notes supérieures à la moyenne de la classe est : {notesup}")
