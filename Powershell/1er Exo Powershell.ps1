547aa0819f4f71699f31c0ba4dbfe45616c4606f
Domandio <m.landuze@gmail.com>
2025-09-10 16:16:10 +0200

    .

--- Powershell/1er Exo Powershell.ps1
+++ Powershell/1er Exo Powershell.ps1
@@ -1,9 +1,14 @@
-﻿#Services
-
+﻿# Afficher les Services
 Get-Help use*
 Get-Service
+
+#Afficher les services avec des contraintes de nom :
 Get-Service -DisplayName "win*"
 Get-Service -Name "win*" -Exclude "*Defend*"
+Get-Service -Name "win*" -DisplayName -Exclude "*Service de*"
+Get-Service -Name "win*" | Where-Object {$_.DisplayName -notlike "*Service de*"}
+
+#Afficher les services avec une contrainte de statut :
 Get-Service -Name "win*" | Where-Object {$_.Status -eq "Stopped"}
 
 #Processus
@@ -11,4 +16,3 @@ Get-Process
 
 Start-Process Firefox
 Stop-Process -name Firefox
-.
