7e6b382e8f6b534ce1bc941cd59bc6e3b475a111
Domandio <m.landuze@gmail.com>
2025-09-10 15:51:44 +0200

    .

---
+++ Powershell/1er Exo Powershell.ps1
@@ -0,0 +1,13 @@
+﻿#Services
+
+Get-Help use*
+Get-Service
+Get-Service -DisplayName "win*"
+Get-Service -Name "win*" -Exclude "*Defend*"
+Get-Service -Name "win*" | Where-Object {$_.Status -eq "Stopped"}
+
+#Processus
+Get-Process
+
+Start-Process Firefox
+Stop-Process -name Firefox
