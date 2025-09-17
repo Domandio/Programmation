b0103ccbf6100370b1e3eb6bc4ff91d3d01de07b
Domandio <m.landuze@gmail.com>
2025-09-17 08:35:01 +0200

    .

---
+++ Bash/Langage script/TD1 Langage script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Exercice 1 ------
+date "+%H:%M:%S %D"
+
+#Exercice 2 ------
+#Créer des variables pour enregistrer les groupes les uns après les autres
+g1=$(groups | cut -d ' ' -f1)
+g2=$(groups | cut -d ' ' -f2)
+g3=$(groups | cut -d ' ' -f3)
+
+#Afficher les groupes ainsi que le formatage
+echo "$g1;$g2;$g3."
