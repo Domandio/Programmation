a5633bd47059a060b625b6301e74c97dfc34d722
Domandio <m.landuze@gmail.com>
2025-09-24 09:59:28 +0200

    .

---
+++ Bash/Langage script/TD/TD2/TD2_All_Langage script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Exercice 1  ------
+
+#Exercice 2  ------
+
+#Exercice 3  ------
+
+#Exercice 4  ------
+
+#Exercice 5  ------
+
+#Exercice 6  ------
+
+#Exercice 7  ------
+
+#Exercice 8  ------
+
+#Exercice 9  ------
+
+#Exercice 10 ------
