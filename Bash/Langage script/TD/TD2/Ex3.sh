3bd5575d54606905b34f297ffda1361abc375206
Domandio <m.landuze@gmail.com>
2025-09-25 12:17:44 +0200

    .

--- Bash/Langage script/TD/TD2/Ex3.sh
+++ Bash/Langage script/TD/TD2/Ex3.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-ls -l | cut -c 1
+nbtotal=$(ls -1 | wc -l)
+
+nbdossier=$(ls -l | cut -c 1 | grep -c "d")
+nbfichiers=$(ls -l | cut -c 1 | grep -c "-")
+
+nbfichierscaches=$(ls -a | grep -c "^\.")
+
+echo "Nombre total d'éléments : $nbtotal" 
+echo "Nombre total de dossiers : $nbdossier"
+echo "Nombre total de fichiers ordinaires : $nbfichiers"
+echo "Nombre total de fichiers cachés : $nbfichierscaches"
