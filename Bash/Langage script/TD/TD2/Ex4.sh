afb862ac7f27e0d7146dffa5d251088371d3f5b8
Domandio <m.landuze@gmail.com>
2025-09-28 10:00:50 +0200

    .

--- Bash/Langage script/TD/TD2/Ex4.sh
+++ Bash/Langage script/TD/TD2/Ex4.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+#Copie de passwd dans le répertoire courant de l'utilisateur
+cp /etc/passwd ~/passwd.txt
+#Modification des droits
+chmod 666 ~/passwd.txt 
+ #Tri par ordre alphabétique décroissant
+ sort -r ~/passwd.txt
