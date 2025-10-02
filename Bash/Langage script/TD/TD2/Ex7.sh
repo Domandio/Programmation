414901957be3e2dc9592f8ca18b0c778e18d1bd6
Domandio <m.landuze@gmail.com>
2025-10-02 09:10:22 +0200

    .

--- Bash/Langage script/TD/TD2/Ex7.sh
+++ Bash/Langage script/TD/TD2/Ex7.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+echo "Entrez le login de l'utilisateur :"
+read login
+#On exécute la commande "id" afin de récupérer son code d'erreur avec $?. Si ce code est à 0 l'utilisateur n'existe pas.
+id "$login"
+if [ $? -ne 0 ]; then
+echo "Erreur : l'utilisateur $login n'existe pas"
+exit
+fi
+#On utilise la commande "id" à nouveau. -G : Affiche les identifiants de groupes -n : Affiche les noms plutôt que des nombres
+echo "Groupes secondaires de $login :"
+id -Gn "$login" | cut -d' ' -f2-
